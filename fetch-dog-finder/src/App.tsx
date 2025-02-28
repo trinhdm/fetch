f5aa2fa202f59c675497e58247621e7b53db968d
Doreen Trinh <doreenmtrinh@gmail.com>
2025-02-28 00:00:30 -0800

    refactor: App routing - added redirecting based on context/state

--- fetch-dog-finder/src/App.tsx
+++ fetch-dog-finder/src/App.tsx
@@ -1,28 +1,21 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { useCallback, useState } from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
+import { UserProvider } from '@providers/UserProvider'
+import { USER_DEFAULTS } from '@utils/constants'
 import { Directory } from '@layout/Directory'
 import { Favorites } from '@layout/Favorites'
 import { Login } from '@layout/Login'
 import { Page } from '@layout/Page'
-import { UserProvider } from '@providers/UserProvider'
 import type { User, UserSettings } from '@typings/shared'
 import '@assets/scss/global.scss'
 import './App.scss'
 
 function App() {
-	const defaultUser: User = {
-		name: '',
-		email: '',
-		isLoggedIn: false,
-		favorites: [],
-		match: {},
-	}
+	const [user, setUser] = useState<User>(USER_DEFAULTS)
 
-	const [user, setUser] = useState<User>(defaultUser)
-
-	const handleUser = (
-		data: Partial<User> = defaultUser
-	) => setUser(state => ({ ...state, ...data }))
+	const handleUser = useCallback((
+		data: Partial<User> = USER_DEFAULTS
+	) => setUser(state => ({ ...state, ...data })), [])
 
 	const userValues: UserSettings = {
 		...user,
@@ -33,14 +26,21 @@ function App() {
 		<UserProvider values={ userValues }>
 			<BrowserRouter>
 				<Routes>
-					{ user.isLoggedIn ? (
-						<>
-							<Route path="/" element={ <Page><Directory /></Page> } />
-							<Route path="/favorites" element={ <Page><Favorites /></Page> } />
-						</>
-					) : (
-						<Route path="/" element={ <Login /> } />
-					) }
+					<Route path="/" element={
+						user.isLoggedIn
+							? <Navigate replace to="/directory" />
+							: <Login />
+					} />
+					<Route path="/directory" element={
+						user.isLoggedIn
+							? <Page><Directory /></Page>
+							: <Navigate replace to="/" />
+					} />
+					<Route path="/favorites" element={
+						user.isLoggedIn
+							? <Page><Favorites /></Page>
+							: <Navigate replace to="/" />
+					} />
 				</Routes>
 			</BrowserRouter>
 		</UserProvider>
